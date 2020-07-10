bb9b794d73ef22d52831e8b90c41d5bd70ee7d74
jebarcha <jebarcha@hotmail.com>
2020-07-10 15:47:52 -0500

    build

---
+++ docs/precache-manifest.ff5c24853fd61a6b89c31703a184d9ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40148a567d20aed61a844cbd6914a42e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9254657c48174a01edc2",
+    "url": "/static/js/2.412a28e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.412a28e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecea57fc4ff7f66788dc",
+    "url": "/static/js/main.8ce962da.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
